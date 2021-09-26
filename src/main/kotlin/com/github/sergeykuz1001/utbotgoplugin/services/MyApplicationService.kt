b4b81ee4ef983be62058973ad4b124f560a689d8
GitHub Action <action@github.com>
2021-09-26 11:57:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sergeykuz1001/utbotgoplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sergeykuz1001.utbotgoplugin.services
+
+import com.github.sergeykuz1001.utbotgoplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
